c8e4f847344b83f58310613db20b4c213bd0f6ec
ANTONIOa7o <78384046+ANTONIOa7o@users.noreply.github.com>
2022-01-20 15:02:45 +0100

    Add files via upload

---
+++ JavaScript_I/Ejemplo3-Fuciones/js/script.js
@@ -0,0 +1,20 @@
+
+var n1= parseInt(window.prompt("Introduce el primer numero"));
+var n2 =parseInt(window.prompt("Introduce el segundo numero"));
+
+function sumar(n1,n2){
+    var resultado = n1+n2;
+    alert("La Suma es: " + resultado);
+
+}
+function restar(n1,n2){
+ alert("La Resta es: " + (n1-n2));
+}
+
+function multiplicar(n1,n2){
+alert("La multiplación es: "+ (n1*n2));
+}
+
+function dividir(n1,n2){
+alert("La division es: "+ (n1/n2));
+}
